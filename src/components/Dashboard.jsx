47e08756af9c32e44aea71919675e6d067ac785e
Yash <yashlaxman@gmail.com>
2024-05-30 18:55:13 +0530

    add challenge card

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Challenge1 from "../challenges/Challenge1";
 import ChallengeCard from "./ChallengeCard";
 
 export default function Dashboard() {
@@ -7,7 +6,11 @@ export default function Dashboard() {
     <div className="flex flex-col justify-start items-start">
       <h1 className="ml-3 mb-10">Welcome!</h1>
       <div className="flex flex-col flex-grow w-full gap-5">
-        <ChallengeCard />
+        <ChallengeCard title={"Challenge 1"} />
+        <ChallengeCard title={"Challenge 2"} />
+        <ChallengeCard title={"Challenge 3"} />
+        <ChallengeCard title={"Challenge 4"} />
+        <ChallengeCard title={"Challenge 5"} />
       </div>
     </div>
   );
