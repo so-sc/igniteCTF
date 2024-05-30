e8cadc28cc0c38c1aa9b31c4f6e1d13442482b7c
Yash <yashlaxman@gmail.com>
2024-05-30 18:55:13 +0530

    add challenge card

--- src/components/ChallengeCard.jsx
+++ src/components/ChallengeCard.jsx
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function ChallengeCard() {
-  return <div className="w-full bg-slate-900 h-16"></div>;
+export default function ChallengeCard({ title }) {
+  return (
+    <div
+      className="w-full bg-slate-900 h-16 flex items-center justify-center  cursor-pointer"
+      onClick={() => console.log("hello")}
+    >
+      <h1 className="text-sm text-center">{title}</h1>
+    </div>
+  );
 }
